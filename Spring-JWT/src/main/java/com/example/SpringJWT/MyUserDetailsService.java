d684d448ea4ba7841bfa0abeb2346e47290a130a
met <64790786+memetrusidovski@users.noreply.github.com>
2020-12-27 13:10:22 -0500

    working

--- Spring-JWT/src/main/java/com/example/SpringJWT/MyUserDetailsService.java
+++ Spring-JWT/src/main/java/com/example/SpringJWT/MyUserDetailsService.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Optional;
+
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
--- Spring-JWT/src/main/java/com/example/SpringJWT/SecurityConfig.java
+++ Spring-JWT/src/main/java/com/example/SpringJWT/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
