0bba1deb1b3f6df1b9d1b4306a70a488a489a7d4
met <64790786+memetrusidovski@users.noreply.github.com>
2020-12-27 11:45:54 -0500

    moreJWT

---
+++ Spring-JWT/src/main/java/com/example/SpringJWT/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.SpringJWT;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        //Optional<User> user = userRepository.findByUserName(userName);
+
+        //user.orElseThrow(() -> new UsernameNotFoundException("User not found: " + userName));
+
+        //return user.map(MyUserDetails::new).get();
+        return new User("test", "pass", new ArrayList<>());
+    }
+}
