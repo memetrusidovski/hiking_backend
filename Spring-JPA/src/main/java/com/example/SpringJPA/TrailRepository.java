06220f865963a8c212d7f0b7827a7c6c44ec0ba9
met <64790786+memetrusidovski@users.noreply.github.com>
2021-01-02 22:36:36 -0500

    added pagable and query derivation

--- Spring-JPA/src/main/java/com/example/SpringJPA/TrailRepository.java
+++ Spring-JPA/src/main/java/com/example/SpringJPA/TrailRepository.java
@@ -1,6 +1,9 @@
 package com.example.SpringJPA;
 
 import com.example.SpringJPA.Models.Trail;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
@@ -8,5 +11,8 @@ import java.util.UUID;
 //The First value in the generic type is the Model object that the CRUD
 //App is working with. The second is the type of the id.
 public interface TrailRepository extends CrudRepository<Trail, UUID> {
+    String findByName(String name);
+    //Page<Trail> findByRating(Integer rating, Pageable page);
+    Slice<Trail> findByRating(Integer rating, Pageable page);
 
 }
